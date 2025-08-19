c6bc929b82ede11482e15da0bc59b5853ea4177e
Nigel Guven <nigelguven@gmail.com>
2025-08-19 03:04:10 +0100

    Navigation Bar clean up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -32,6 +33,7 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule,
     BrowserAnimationsModule,
     ModalModule.forRoot(),
     CarouselModule.forRoot(),
